8925f1042a6d69f4438bd0e818985b22c73568d8
OptimalFailure <54728770+OptimalFailure@users.noreply.github.com>
2025-02-28 15:48:37 -0600

    Corrected typos

--- Messengers.py
+++ Messengers.py
@@ -7,60 +7,60 @@ You should NOT modify ANY code in this file.
 """
 
 class Ex1Messenger:
-    def request_base() -> str:
+    def request_base(self) -> str:
         """Request the base from the user with appropriate message. This function does NOT modify or validate the string"""
-        return input("Inter a base: ")
+        return input("Enter a base: ")
 
-    def request_base() -> str:
+    def request_power(self) -> str:
         """Request the power from the user with appropriate message. This function does NOT modify or validate the string"""
-        return input("Inter a power: ")
+        return input("Enter a power: ")
     
-    def print_answer(result:int) -> None:
+    def print_answer(self, result:int) -> None:
         """Display the calculated answer in appropriate format"""
         print(f"Answer: {result}")
     
-    def warn_exponent_out_of_range() -> None:
+    def warn_exponent_out_of_range(self) -> None:
         """Print the appropriate warning for a negative exponent"""
         print("Sorry, your exponent must be zero or larger.")
 
-    def warn_invalid_type() -> None:
+    def warn_invalid_type(self) -> None:
         """Print the appropriate warning for a invalid base/exponent type"""
         print("Sorry, base and exponent must be ints.")
     
 
 class Ex2Messenger:
-    def request_sick_count() -> str:
+    def request_sick_count(self) -> str:
         """Request the day from the user with appropriate message. This function does NOT modify or validate the string"""
         return input("OUTBREAK!\nWhat day do you want a sick count for?: ")
     
-    def print_sick_count(count:int) -> None:
+    def print_sick_count(self, count:int) -> None:
         """Display the calculated answer in appropriate format"""
         print(f"Total people with flu: {count}")
 
-    def warn_invalid_day() -> None:
+    def warn_invalid_day(self) -> None:
         """Print the appropriate warning for an invalid day"""
         print("Invalid day")
     
 
 class Ex3Messenger:
-    def request_mode_and_value() -> str:
+    def request_mode_and_value(self) -> str:
         """Request the mode and value from the user with appropriate message. This function does NOT modify or validate the string"""
         return input("Enter mode and value: ") 
     
-    def print_number(num:int) -> None:
+    def print_number(self, num:int) -> None:
         """Print a fibonacci number in the appropriate format"""
         print(num)
     
-    def print_verification(num:int, isInSequence:bool) -> None:
+    def print_verification(self, num:int, isInSequence:bool) -> None:
         """Print whether or not a number is a fibonacci number, in the appropriate format"""
         res = "is" if isInSequence else "is not"
         print(f"{num} {res} in the sequence")
 
-    def warn_invalid_flag() -> None:
+    def warn_invalid_flag(self) -> None:
         """Print the appropriate warning for invalid flag"""
         print("Invalid input. Flag must be -i or -v")
 
-    def warn_invalid_num() -> None:
+    def warn_invalid_num(self) -> None:
         """Print the appropriate warning for invalid number"""
         print("Invalid input. With -i flag, number must be an integer >= 0")
 
